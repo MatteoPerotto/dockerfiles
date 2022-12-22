dad39d6788d88f33fe831916fa54d2eeceedb36d
Matteo Perotto <perotto.matteo@gmail.com>
2022-12-22 14:17:09 +0100

    Last changes

--- dockerfiles-dGPIS/run.sh
+++ dockerfiles-dGPIS/run.sh
@@ -5,7 +5,7 @@ xhost +
 docker run \
     -it \
     --name $1 \
-    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
     -e DISPLAY=$DISPLAY \
     -e NVIDIA_DRIVER_CAPABILITIES=all \
     --gpus all \
